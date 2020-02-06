1eb1d62de8645a276b3648e6c4ffdfcc3c1f5bdd
jfbm74 <jfbm74@gmail.com>
2020-02-06 00:21:33 +0000

    Uploading task 5 Write a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,11 @@
 /**
 * main - runs the all the code
 *
-* Description: Run puts and print statement
+* Description: Run printf and print statements
 * Return: Return 0 to exit function
 */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
