8e32128c2d53bc03de26921644ff6b1cc9fb2a5d
jfbm74 <jfbm74@gmail.com>
2020-02-06 17:30:12 +0000

    Uploading task 1

---
+++ 0x01-variables_if_else_while/templatec.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* Main runs the whole code
+*Description:This program will assign a random number
+*to the variable n each time it is executed.
+*Complete the source code in order to print whether
+*the number stored in the variable n is positive or negative
+*Return: Return 0 to exit function
+Author: JUAN FELIPE BUSTAMANTE jfbm74@gmail.com
+*/
+int main(void)
+{
+
+	return (0);
+}
