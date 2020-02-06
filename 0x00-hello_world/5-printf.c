3bc095bef9490d1936ef307841a559ba844634fa
jfbm74 <jfbm74@gmail.com>
2020-02-06 00:16:50 +0000

    solving issue at  task 4 Write a C program that prints exactly

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - runs the all the code
+*
+* Description: Run puts and print statement
+* Return: Return 0 to exit function
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
