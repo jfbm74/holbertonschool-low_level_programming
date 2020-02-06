7764563c52d7e388fd27cabb343bda2894fbf5a6
jfbm74 <jfbm74@gmail.com>
2020-02-06 20:38:33 +0000

    uploading task 1

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Check sign on int
+*
+* Return: 0
+*/
+int main(void)
+{
+	int n;
+	unsigned int lastone;
+
+	/*Generating a random number*/
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/*print the last digit of the number stored in the variable n*/
+	lastone = n % 10;
+	if (lastone > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastone);
+	} else if (lastone == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, lastone);
+	} else if (lastone < 6)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastone);
+	}
+	return (0);
+}
