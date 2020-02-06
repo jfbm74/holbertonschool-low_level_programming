f88562e47d1b413e065696dffc2417eb2263f1db
jfbm74 <jfbm74@gmail.com>
2020-02-06 21:07:36 +0000

    uploading task 3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Prints the alphabet in lowercase
+*
+* Return: 0
+*/
+int main(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		putchar (c);
+	}
+	putchar ('\n');
+	return (0);
+}
