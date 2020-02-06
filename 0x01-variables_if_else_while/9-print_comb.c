78b449ec951cfb942e2c678bcdc8f1703d9eb617
jfbm74 <jfbm74@gmail.com>
2020-02-06 22:58:58 +0000

    Adding task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Prints the digit numbers 0-9
+*
+* Return: 0
+*/
+int main(void)
+{
+	int c = '0';
+
+	while (c < '9')
+	{
+			putchar(c);
+			putchar(',');
+			putchar(' ');
+			c++;
+	}
+	putchar(c);
+	return (0);
+}
