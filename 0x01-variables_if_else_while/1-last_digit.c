975000f59014bffb1e5c6ecede9d6b8f47d781b7
jfbm74 <jfbm74@gmail.com>
2020-02-06 20:42:42 +0000

    uploading task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ int main(void)
 	} else if (lastone == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, lastone);
-	} else if (lastone < 6 && lastone !=0)
+	} else if (lastone < 6 && lastone != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastone);
 	}
