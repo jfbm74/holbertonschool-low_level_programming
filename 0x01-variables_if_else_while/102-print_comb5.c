f356ce58b1a990ee06d0929ab91b7d6f214cd202
jfbm74 <jfbm74@gmail.com>
2020-02-07 04:07:09 +0000

    Adding task 12 - 13

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Prints the digit numbers 0-9
+*
+* Return: 0
+*/
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	for (i = '0' ; i <= '9' ; i++)
+	{
+		for  (j = '0' ; j <= '8' ; j++)
+		{
+			for (k = i ; k <= '9' ; k++)
+			{
+				for (l = j + 1 ; l<= '9' ; l++)
+				{
+						putchar(i);
+						putchar(j);
+						putchar(' ');
+						putchar(k);
+						putchar(l);
+						putchar(',');
+						putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
