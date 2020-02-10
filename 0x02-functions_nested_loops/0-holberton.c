71bd5d39492edcde8c1ffd774326214a566781ca
jfbm74 <jfbm74@gmail.com>
2020-02-10 22:25:21 +0000

    Uploading ReadmeFile

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - program that prints Holberton, followed by a new line.
+*
+* Return: 0
+*/
+int main(void)
+{
+	printf ("Holberton\n");
+	return (0);
+}
