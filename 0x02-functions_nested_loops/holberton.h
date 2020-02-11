3e18cae9c1484b990b55702ba80d6d01663f2fcb
jfbm74 <jfbm74@gmail.com>
2020-02-11 15:40:06 +0000

    Program that prints the last digit of a determined number

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,8 +1,8 @@
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int c);
+int _islower(int);
+int _isalpha(int);
+int print_sign(int);
 int _abs(int);
 int print_last_digit(int);
