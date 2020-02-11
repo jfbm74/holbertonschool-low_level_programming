dd63f0c76888993f343b152ef1e1be0c71fab1fc
jfbm74 <jfbm74@gmail.com>
2020-02-11 15:32:10 +0000

    Program that prints the last digit of a determined number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,15 +1,16 @@
 #include "holberton.h"
 /**
-*  print_last_digit - program that prints last digit value
-* Return: n % 10 - absolute value of n
-*  @n: number to check
+* print_last_digit - print last digit of an integer
+* @n: the integer to print the last digit of
+*
+* Return: Value of last digit
 */
 int print_last_digit(int n)
 {
 	if ((n % 10) < 0)
 	{
-		n *= -1;
+		n = n * -1;
 	}
-	_putchar((n % 10) + '0');
+	_putchar((n % 10)+ '0');
 	return (n % 10);
 }
