1ad8eb3c1188a088b88b2f212dd34deaaafdf76f
jfbm74 <jfbm74@gmail.com>
2020-02-11 22:57:31 +0000

    Adding readme file, 0-main.c holberton.h for task 0 - debugging

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Check sign on int
+*
+* Return: 0
+*/
+void positive_or_negative(int n)
+{
+
+	if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	} else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+}
