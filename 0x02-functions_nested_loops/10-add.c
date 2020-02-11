7ca3c6226fdca043301a060e4029f4ace0fe1b52
jfbm74 <jfbm74@gmail.com>
2020-02-11 21:01:29 +0000

    Adding task 10 - Adding two values

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+*  add- prints mult tables
+* @a : first value
+* @b : second value
+* : result of adding a + b
+*
+* Return: Add a+b
+*/
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
