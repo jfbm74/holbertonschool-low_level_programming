50c00240f299b7bc4d40bdb5c6fa94bd256d9c0f
jfbm74 <jfbm74@gmail.com>
2020-02-12 03:54:39 +0000

    Uploading task 12 - n times table

--- 0x02-functions_nested_loops/bk100.c
+++ 0x02-functions_nested_loops/bk100.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+
 /**
 * print_times_table - prints mult tables
 * @n  : the integer to print the times tables
@@ -12,8 +13,7 @@ void print_times_table(int n)
 
 	if (!((n >= 0) ^ (n <= 15)))
 	{
-		for (i = 0 ; i <= n ; i++)
-		{
+		for (i = 0 ; i <= n ; i++){
 			for (j = 0 ; j <= n ; j++)
 			{
 				result = i * j;
