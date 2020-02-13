7f9449639b248e240c80e8f10d01c4b9b91c1713
jfbm74 <jfbm74@gmail.com>
2020-02-13 17:23:04 +0000

    Solving problem with comments

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,14 +2,12 @@
 
 /**
 * print_numbers - print 0-9 on standard output
-* @c: given value
-* @b:  
 * Return: product
 */
 void print_numbers(void)
 {
 	int i;
-	
+
 	for (i = 0 ; i < 10 ; i++)
 	{
 		_putchar(i + '0');
