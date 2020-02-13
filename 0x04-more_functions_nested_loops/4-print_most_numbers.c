a0c29ee3bcef29dadec8fd28f47b4d1276b11458
jfbm74 <jfbm74@gmail.com>
2020-02-13 17:24:49 +0000

    Solving problems with blank spaces

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,14 +2,12 @@
 
 /**
 * print_most_numbers - print 0-9 2&4 are excluded
-* 
-*   
 * Return: product
 */
 void print_most_numbers(void)
 {
 	int i;
-	
+
 	for (i = 0 ; i < 10 ; i++)
 	{
 		if (i != 2 && i != 4)
