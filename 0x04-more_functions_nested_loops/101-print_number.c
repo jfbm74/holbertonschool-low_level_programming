0c52a59f92fc85a3d61c6d9cd13a840e9e9b49f2
jfbm74 <jfbm74@gmail.com>
2020-02-15 04:25:18 +0000

    Solving Task 12 - Print integer with _putchar

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,51 @@
+#include "holberton.h"
+
+/**
+ * print_number - Print any number one character at a time
+ * @n: Number to print
+ *
+ * Return: Nothing
+ */
+void print_number(int n)
+{
+	int res;
+	int x;
+
+	x = n;	
+	if (x < 0)
+	{
+		_putchar('-');
+		x *= -1;
+	}
+	if (x / 100000 != 0)
+	{
+		res = x / 100000;
+		_putchar(res / 10000 % 10 + '0');
+		_putchar(res / 1000 % 10 + '0');
+		_putchar(res / 100 % 10 + '0');
+		_putchar(res / 10 % 10 + '0');
+		_putchar(res % 10 + '0');
+	}
+	res = x % 100000;
+	if (res / 10000 % 10 != 0)
+	{
+		_putchar(res / 10000 % 10 + '0');
+		_putchar(res / 1000 % 10 + '0');
+		_putchar(res / 100 % 10 + '0');
+		_putchar(res / 10 % 10 + '0');
+	}
+	else if (res / 1000 % 10 != 0)
+	{
+		_putchar(res / 1000 % 10 + '0');
+		_putchar(res / 100 % 10 + '0');
+		_putchar(res / 10 % 10 + '0');
+	}
+	else if (res / 100 % 10 != 0)
+	{
+		_putchar(res / 100 % 10 + '0');
+		_putchar(res / 10 % 10 + '0');
+	}
+	else if (res / 10 % 10 != 0)
+		_putchar(res / 10 % 10 + '0');
+	_putchar(res % 10 + '0');
+}
