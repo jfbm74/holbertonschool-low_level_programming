ba5addff32f1c47622d65694177679706b629c42
JuanFe <jfbm74@gmail.com>
2020-02-19 04:35:12 +0000

    Task 5  Reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+void rev_string(char *s)
+{
+	int len;
+	int i;
+	int j;
+	char tmp;
+
+	len = 0;
+	while (s[len] != 0)
+	{
+		len++;
+	}
+	len = len - 1;
+	for (i = 0 ; i < j ; i++)
+	{
+		for (j = len ; j > i ; j--)
+		{
+			tmp = s[j];
+			s[j] = s[i];
+			s[i] = tmp;
+		}
+	}
+}
