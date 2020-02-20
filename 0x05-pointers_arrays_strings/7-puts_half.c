e64ed66d3ece8853b7ba4a1ed15411d459d93954
JuanFe <jfbm74@gmail.com>
2020-02-20 01:02:24 +0000

    print half string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
- * rev_string - Print a string in rev
- * @s: String to print
+ * puts_half - Print a string in rev
+ * @str: String to print
  *
  * Return: nothing
  */
@@ -9,23 +9,13 @@ void puts_half(char *str)
 {
 	int len;
 	int i;
-	
+
 	len = 0;
 	for (i = 0; str[i] != '\0' ; i++)
 		len++;
-	/*if (len % 2 == 0)
-	{*/
-		for (i = (len /2) ;  str[i] != '\0'; i++)
+		for (i = (len / 2) ;  str[i] != '\0'; i++)
 		{
 			_putchar(str[i]);
 		}
-	/*}
-	else
-	{
-		for (i = ((len / 2) - 1); str[i] != '\0'; i++)
-		{
-			_putchar(str[i]);
-		}
-	}*/
 	_putchar('\n');
 }
