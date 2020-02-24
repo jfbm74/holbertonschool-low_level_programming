82c97742b2f738ea60ba0f8681f36273756cbec2
JuanFe <jfbm74@gmail.com>
2020-02-24 17:22:56 +0000

    Function that locates a char in string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,8 +1,9 @@
 /**
- *
- *
- *
- */
+* _strchr - function that locates a char in string
+* @s: given string
+* @c: character to locate on string
+* Return: return pointer to points a position of char
+*/
 char *_strchr(char *s, char c)
 {
 	int i;
