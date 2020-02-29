8b9597cb5479ef889b13c6c88b4e320b6b254827
JuanFe <jfbm74@gmail.com>
2020-02-29 17:06:44 +0000

    Adding task 9, function that copies the string pointed to by src

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,19 +1,19 @@
 /**
- *
- *
- *
- *
+ * _strcpy - function that copies the string pointed to by src
+ * @dest: target string
+ * @src: source string
+ * Return: string pointing to dest
  */
 char *_strcpy(char *dest, char *src)
 {
 	int i;
 	int j;
-	
+
 	i = j = 0;
-	for (i = 0; src[i] !='\0'; i++)
+	for (i = 0; src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
-	dest[i + 1]= '\0';
+	dest[i + 1] = '\0';
 	return (dest);
 }
