f0931faf9c0f206c1c24f6556b38b8da25905129
jfbm74 <jfbm73@gmail.com>
2020-03-01 04:40:01 +0000

    program that adds positive numbers.

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -  program that adds positive numbers.
+ * @argc: args counter
+ * @argv: args array
+ * Return: 1=success  0=failure
+ */
+int main(int argc __attribute__((unused)), char **argv)
+{
+	unsigned long i;
+	unsigned long k;
+	int sum;
+
+	sum = 0;
+	k = argc;
+	for (i = 1; i < k ; i++)
+	{
+	if (!atoi(argv[i]))
+		{
+			printf("Error\n");
+			return (EXIT_FAILURE);
+		}
+		else
+		{
+			sum = sum + atoi(argv[i]);
+		}
+	}
+	printf("%d\n", sum);
+	return (EXIT_SUCCESS);
+}
