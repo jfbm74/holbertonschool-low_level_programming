0f5a5895ca0f84e7b85e1d84ed577cff5e324148
jfbm74 <jfbm73@gmail.com>
2020-03-01 16:41:06 +0000

    program that prints a quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@ int main(void)
 {
 	int i;
 
-	i= sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	i = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", i);
 	return (1);
 }
