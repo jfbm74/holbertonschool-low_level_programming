a2ebaafa058beaace203655b0fc9a9b158dffc03
jfbm74 <jfbm73@gmail.com>
2020-03-03 22:27:56 +0000

    returns a pointer to a newly allocated space in memory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory
+ * @str: given string
+ * Return: new string copy of str
+ */
+char *_strdup(char *str)
+{
+	int i;
+	int s_str;
+	char *copy;
+
+	i = s_str = 0;
+	if (str == NULL)
+	{
+		exit(EXIT_FAILURE);
+	}
+	while (str[i] != '\0')
+	{
+		s_str++;
+		i++;
+	}
+	copy = malloc(s_str * sizeof(char));
+	if (copy == NULL)
+	{
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		i = 0;
+		while (i <= s_str)
+		{
+			copy[i] = str[i];
+			i++;
+		}
+	}
+	return (copy);
+	free(copy);
+}
