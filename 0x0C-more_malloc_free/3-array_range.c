91589a1a0f7618d0e7c1d535180b0d08ac5c151b
jfbm74 <jfbm73@gmail.com>
2020-03-05 22:47:45 +0000

    function that creates an array of integers.

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,8 +1,10 @@
 #include <stdlib.h>
+#include <stdio.h>
 /**
- *
- *
- *
+ * array_range - Function that creates an array of integers.
+ * @min: number to start in array
+ * @max: number to finish array
+ * Return: array
  */
 int *array_range(int min, int max)
 {
@@ -12,14 +14,15 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	for (i =min; i < max ; i++)
+	for (i = min; i < max ; i++)
 		len++;
-	ar = malloc(len * sizeof(int));
+	printf("len= %d\n", len);
+	ar = malloc((len * sizeof(int) + 1));
 	if (ar == NULL)
 		return (NULL);
 	for (i = min; i <= max; i++)
 	{
-		ar[i] = min;
+		ar[i] = i;
 	}
 	return (ar);
 }
--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -1,6 +1,5 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-#include <stdio.h>
 int _putchar(char c);
 int _isalpha(int c);
 int _abs(int n);
@@ -29,4 +28,5 @@ char *argstostr(int ac, char **av);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
 #endif /* HOLBERTON_H */
