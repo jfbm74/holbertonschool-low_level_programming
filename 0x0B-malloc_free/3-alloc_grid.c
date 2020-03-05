eeaaaa06143d0355de9ece8103981f4b410af9ff
jfbm74 <jfbm73@gmail.com>
2020-03-05 00:26:38 +0000

    fixing edge cases

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,34 +1,34 @@
 #include <stdlib.h>
-#include <stdio.h>
 /**
- * alloc_grid - turns a pointer to a 2 dim array of integers
- * @width: rows
- * @height: columns
- * Return: pointer
+ * *alloc_grid - Returns a ptr to a 2 dime array of integers.
+ * @width: array cols
+ * @height: array rows
+ * Return: array
  */
 int **alloc_grid(int width, int height)
 {
-	int **p;
-	int i;
-	int j;
+	int **ar;
+	int i, j;
 
-	p = malloc(height * sizeof(int *));
-	if (p == NULL)
+	if (width < 0 || height < 0)
 		return (NULL);
-	for (i = 0; i < height; i++)
+	ar = malloc(height * sizeof(int *));
+	if (ar ==  NULL)
+		return (NULL);
+	for (i = 0; i < height ; i++)
 	{
-		p[i] = malloc(width * sizeof(int));
-		if (p[i] == NULL)
+		ar[i] = malloc(width * sizeof(int));
+		if (ar[i] == NULL)
 		{
-			return (NULL);
+			for (j = i - 1 ; j > 0; j--)
+			{
+				free(ar[i]);
+			}
 		}
 		for (j = 0; j < width ; j++)
 		{
-			p[i][j] = 0;
+			ar[i][j] = 0;
 		}
 	}
-	return (p);
-	for (i = 0; i < width; i++)
-		free(p[i]);
-	free(p);
+	return (ar);
 }
