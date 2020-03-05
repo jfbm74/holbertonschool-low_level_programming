0d6860f1c30cc8aa756a3823f2a04cbb269a8b8c
jfbm74 <jfbm73@gmail.com>
2020-03-05 23:31:28 +0000

    fixing issue task 3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 /**
  * array_range - Function that creates an array of integers.
  * @min: number to start in array
@@ -11,16 +12,17 @@ int *array_range(int min, int max)
 	int i;
 	int len;
 
-	if (min > max)
+	len = min + max + 1;
+	if (min >= max)
 		return (NULL);
-	for (i = min; i < max ; i++)
-		len++;
-	ar = malloc((len * sizeof(int) + 1));
+	ar = malloc(len * sizeof(int));
 	if (ar == NULL)
 		return (NULL);
-	for (i = min; i <= max; i++)
+	/*Asign number to array[i]*/
+	for (i = 0; min <= max; i++)
 	{
-		ar[i] = i;
+		ar[i] = min;
+		min++;
 	}
 	return (ar);
 }
