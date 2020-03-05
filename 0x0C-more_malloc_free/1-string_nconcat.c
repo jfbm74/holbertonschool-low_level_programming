0c8866c3bf089ccdfd7517a581fabb8e7d3d83aa
jfbm74 <jfbm73@gmail.com>
2020-03-05 19:56:14 +0000

    Fixing null malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -39,6 +39,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if ((int)n >= ls2)
 		n = ls2;
 	str = malloc((ls1 + n + 1) * sizeof(int));
+	if (str == NULL)
+		return (NULL);
 	for (i = 0; i < ls1 ; i++)
 		str[i] = s1[i];
 	for (j = 0; j < (int)n ; j++)
