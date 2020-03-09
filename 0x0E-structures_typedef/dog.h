c18a081b8b2d53486436127aefb7045d30a82537
jfbm <jfbm74@gmail.com>
2020-03-09 16:56:07 -0500

    Define a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Entity Dog
+ * @name: Puppys Name
+ * @age: Puppys Age
+ * @owner: Puppy owner
+ *
+ * Description: Defining char of a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
