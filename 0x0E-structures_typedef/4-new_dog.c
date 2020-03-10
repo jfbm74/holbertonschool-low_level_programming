0612e1ba0350161c4b844fc2b1c437a76cdcffd3
jfbm <jfbm74@gmail.com>
2020-03-09 20:24:39 -0500

    t creates a new dog.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,24 @@
 #include <stdlib.h>
 #include "dog.h"
 /**
+ * _strcpy - Copy the given string
+ * @dest: Location to copy string to
+ * @src: String to copy to given location
+ * Return: Pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (*(src + i) != 0)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	*(dest + i) = '\0';
+	return (dest);
+}
+/**
  * new_dog - Function creates a new dog.
  * @name: puppys name
  * @age: puppys age
@@ -12,6 +30,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *copito;
 	int lname;
 	int lowner;
+	char *new_name;
+	char *new_owner;
 
 	for (lname = 0; name[lname] != '\0'; lname++)
 		;
@@ -21,21 +41,23 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (copito == NULL)
 		return (NULL);
 
-	copito->name =  malloc(sizeof(lname + 1));
-	if (copito->name == NULL)
+	new_name =  malloc(sizeof(lname + 1));
+	if (new_name == NULL)
 	{
 		free(copito);
 		return (NULL);
 	}
-	copito->owner = malloc(sizeof(lowner + 1));
-	if (copito->owner == NULL)
+	new_owner = malloc(sizeof(lowner + 1));
+	if (new_owner == NULL)
 	{
-		free(copito->name);
+		free(new_name);
 		free(copito);
 		return (NULL);
 	}
-	copito->name = name;
-	copito->owner = owner;
+	_strcpy(new_name, name);
+	_strcpy(new_owner, owner);
+	copito->name = new_name;
+	copito->owner = new_owner;
 	copito->age = age;
 	return (copito);
 }
