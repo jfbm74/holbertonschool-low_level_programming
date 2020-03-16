9dbc7af9687e55a71e378c83124eab7ada10ee4f
jfbm74 <jfbm73@gmail.com>
2020-03-16 02:39:42 +0000

    Fixing edge cases for task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,9 +10,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list args;
+	int res;
 
 	va_start(args, n);
 
+	res = 0;
 	if (separator == NULL)
 	{
 		;
@@ -21,12 +23,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		for (i = 0; i < n; i++)
 		{
-			printf("%d", va_arg(args, int));
+			res = va_arg(args, int);
+			printf("%d", res);
 			if (i < (n - 1))
 			{
 				printf(", ");
 			}
 		}
 		printf("\n");
+		va_end(args);
 	}
 }
