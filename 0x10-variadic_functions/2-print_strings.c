b3928637d29fbea90ab39176fcf3997a4811b29e
jfbm74 <jfbm73@gmail.com>
2020-03-16 02:47:36 +0000

    fixing betty style task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,15 +1,17 @@
 #include <stdarg.h>
 #include <stdio.h>
 /**
- * 
- * 
- * 
- * 
-*/void print_strings(const char *separator, const unsigned int n, ...)
+ * print_strings - prints strings, followed by a new line
+ * @separator: separator to print between the strings
+ * @n: number of strings to print
+ * Return: nothing
+*/
+void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
-	char * str;
+	char *str;
 	va_list args;
+
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
@@ -23,7 +25,7 @@
 		{
 			printf("(nil)");
 		}
-		if (i < (n-1))
+		if (i < (n - 1))
 		{
 			printf("%s", separator);
 		}
