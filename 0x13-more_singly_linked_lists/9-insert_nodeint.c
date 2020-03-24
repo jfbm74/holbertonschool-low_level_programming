94677df518fcfda2c814888cfc5e22ce05962425
jfbm74 <jfbm74@gmail.com>
2020-03-24 21:37:02 +0000

    fixing issues on  Task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,21 +10,21 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *tmp = NULL;
 	listint_t *pivot = NULL;
-	listint_t *new;
+	listint_t *new = NULL;
 	unsigned int i = 0;
 
 	if (*head == NULL)
 		return (NULL);
-
-	tmp = (*head);
 	new = malloc(sizeof(listint_t));
 	if (!new)
 		return (NULL);
+	tmp = (*head);
 	new->n = n;
 	if (idx == 0)
 	{
-		new->next = (*head)->next;
+		new->next = tmp;
 		*head = new;
+		return (new);
 	}
 	while (tmp)
 	{
