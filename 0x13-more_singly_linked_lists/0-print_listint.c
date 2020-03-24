8679983093b7c0f3bebb87065f71c8160c43ede3
jfbm74 <jfbm73@gmail.com>
2020-03-24 14:54:27 +0000

    Fixing Checker issues

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,9 +1,11 @@
 #include "lists.h"
+
 /**
  * print_listint - prints all the elements of a listint_t list.
  * @h: List Head
  * Return: Numbers of nodes
 */
+
 size_t print_listint(const listint_t *h)
 {
 	size_t n_nodes;
