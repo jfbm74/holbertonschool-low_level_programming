ec52821f9387ad12df0cf4a20ee425c57c374471
jfbm74 <jfbm74@gmail.com>
2020-03-24 21:41:47 +0000

    fixing issues on  Task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -35,8 +35,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			tmp->next = new;
 			return (new);
 		}
-		tmp = tmp->next;
 		i++;
+		tmp = tmp->next;
 	}
 	return (NULL);
 }
