e8685178802c5c5cc1baaae0f26837441d21f11e
jfbm74 <jfbm74@gmail.com>
2020-03-27 18:16:07 -0500

    traying to fix code

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,9 +1,10 @@
 #include "holberton.h"
+
 /**
- * print_binary - prints binary representation of a number
- * @n: Given number
- * Return: Nohing
+ * print_binary - prints the binary representation of a number
+ * @n: base 10 number
  */
+
 void print_binary(unsigned long int n)
 {
 	if (n < 2)
