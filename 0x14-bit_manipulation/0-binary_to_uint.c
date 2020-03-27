93294332d925e171d88e076a0cf1eeb67a3fcc93
jfbm74 <jfbm74@gmail.com>
2020-03-27 14:24:30 -0500

    Fixing Betty Style

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,8 +3,8 @@
  * binary_to_uint - function that converts a binary number to an unsigned int.
  * @b: given binary number
  * Return: he converted number or 0 if
- * 	there is one or more chars in the string b that is not 0 or 1
- * 	b is NULL
+ * there is one or more chars in the string b that is not 0 or 1
+ * b is NULL
 */
 unsigned int binary_to_uint(const char *b)
 {
