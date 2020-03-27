47fd39d9928041fc7567fe074316b75dd5d49f11
jfbm74 <jfbm74@gmail.com>
2020-03-27 17:36:36 -0500

    Fixing Betty Style

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,14 +6,11 @@
  */
 void print_binary(unsigned long int n)
 {
-	/*unsigned int remainder, num;*/
-
 	if (n < 2)
 	{
 		_putchar(n + '0');
 		return;
 	}
 	print_binary(n >> 0x01);
-
 	_putchar((n & 0x01) + '0');
 }
