948326e0d4ebb95794def7f0c3c5848b22c9cdf9
jfbm74 <jfbm74@gmail.com>
2020-05-01 12:24:24 -0500

    Adding tasj 4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ * free_dlistint - function that free a dlistint_t list.
+ * @head: First node of the list
+ * Return: nothing
+*/
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *pivot;
+
+	pivot = head;
+	if (head == NULL)
+		return;
+
+	if (pivot->next ==  NULL)
+	{
+		free(pivot);
+	}
+	while (pivot->next !=  NULL)
+	{
+		head = head->next;
+		free(pivot);
+		pivot = head;
+	}
+	free(head);
+}
