117fd887871a847ce4d76e185213734e52790f4c
jfbm74 <jfbm74@gmail.com>
2020-05-02 17:46:29 -0500

    Fixing task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,44 +10,37 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int i = 1;
-	unsigned int len_dlist = 0;
 	dlistint_t *pivot;
 	dlistint_t *current;
 	dlistint_t *new;
+	unsigned int i = 1;
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
-
+	/* Base Case */
 	if (*h == NULL || idx == 0)
-	{
-		add_dnodeint(h, n);
-		return (new);
-	}
-	new->n = n;
+		return (add_dnodeint(h, n));
+	/* Insert a last node */
 	pivot = *h;
 	current = *h;
-	while (i < idx)
+	while (i <= idx)
 	{
-		if (current == NULL)
-			return (*h);
 		pivot = pivot->next;
 		current = current->next;
 		i++;
 	}
-	pivot = pivot->next;
+	/* If idx greater than number of nodes*/
 	if (pivot == NULL)
-	{
-		add_dnodeint_end(h, n);
-		return (new);
-	}
-	else if (pivot != NULL)
-	{
-		new->next = pivot;
-		new->prev = current;
-		current->next = new;
-		pivot->prev = new;
-	}
+		return (NULL);
+	/* If is the last node */
+	if (pivot->next == NULL)
+		return (add_dnodeint_end(h, n));
+	new->n = n;
+	current = current->prev;
+	new->next = pivot;
+	new->prev = current;
+	current->next = new;
+	pivot->prev = new;
 	return (new);
 }
