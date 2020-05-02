78fb5f51c32c51cad1f3a7dad9aebb02af7705be
jfbm74 <jfbm74@gmail.com>
2020-05-02 14:58:20 -0500

    Fixing head->prev assign new

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -9,7 +9,7 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new;
+	dlistint_t *new = NULL;
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
@@ -23,6 +23,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		*head = new;
 		return (new);
 	}
+	(*head)->prev = new;
 	new->next = *head;
 	new->prev = NULL;
 	*head = new;
