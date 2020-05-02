8fc1ca09d63bb4b96c18972c474112c9aec8d8b1
jfbm74 <jfbm74@gmail.com>
2020-05-02 15:21:12 -0500

    Fixing Task 4

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,18 +1,17 @@
 #include "lists.h"
+
 /**
  * free_dlistint - function that free a dlistint_t list.
- * @head: First node of the list
- * Return: nothing
-*/
+ * @head: pointer to the head of the dlistint_t list.
+ *
+ * Return: void.
+ */
 
 void free_dlistint(dlistint_t *head)
 {
-	dlistint_t *pivot;
-
 	if (head == NULL)
 		return;
-
-	while (head->next !=  NULL)
+	while (head->next != NULL)
 	{
 		head = head->next;
 		free(head->prev);
