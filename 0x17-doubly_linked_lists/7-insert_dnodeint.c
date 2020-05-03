803750771597dffab6ae206f055d6158bebe9128
jfbm74 <jfbm74@gmail.com>
2020-05-02 21:59:10 -0500

    Fixing Function Task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -17,6 +17,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (h == NULL || (*h == NULL && idx > 0))
 		return (NULL);
+	if (*h == NULL || idx == 0)
+		return (add_dnodeint(h, n));
 	pivot = *h;
 	current = *h;
 	while (pivot->next != NULL)
