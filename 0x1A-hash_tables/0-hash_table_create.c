bb80b187e5d1cc06a747b5d75854708968dacdb8
jfbm74 <jfbm74@gmail.com>
2020-05-29 10:41:45 -0500

    Addings Hash Table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,29 +1,27 @@
 #include "hash_tables.h"
-
 /**
  * hash_table_create - creates a hash table
  * @size: size
  * Return: pointer to the new hash table
  */
-
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht = NULL;
+	hash_table_t *hashtable = NULL;
 
 	if (size == 0)
 		return (NULL);
 
-	ht =  calloc(1, sizeof(hash_node_t));
-	if (ht == NULL)
+	hashtable = calloc(1, sizeof(hash_table_t));
+	if (hashtable == NULL)
 		return (NULL);
 
-	ht->size = size;
-	ht->array = calloc(size, sizeof(hash_node_t *));
-	if (ht->array == NULL)
+	hashtable->size = size;
+
+	hashtable->array = calloc(size, sizeof(hash_node_t *));
+	if (hashtable->array == NULL)
 	{
-		free(ht);
+		free(hashtable);
 		return (NULL);
 	}
-
-	return (ht);
+	return (hashtable);
 }
